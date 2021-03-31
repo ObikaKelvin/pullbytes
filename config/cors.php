122986580954a5001b4f4df6666e2d0c0cd49ebf
ObikaKelvin <obikakelvin.2255@gmail.com>
2021-03-31 22:37:14 +0100

    updated cors support

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
@@ -27,7 +27,7 @@ return [
 
     'exposed_headers' => ['*'],
 
-    'max_age' => 0,
+    'max_age' => 60,
 
     'supports_credentials' => false,
 
