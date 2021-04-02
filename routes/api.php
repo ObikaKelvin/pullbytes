90e45241f13f441f17fde4a0ad6f47112cd5d2ad
ObikaKelvin <obikakelvin.2255@gmail.com>
2021-04-02 19:28:17 +0100

    fixed http method for verify license route

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/v1/login', 'App\Http\Controllers\AuthController@login')->middleware('api');
 Route::post('/v1/register', 'App\Http\Controllers\AuthController@register')->middleware('api');
-Route::get('/v1/verify_license', 'App\Http\Controllers\LicenseController@verifyLicense');
+Route::post('/v1/verify_license', 'App\Http\Controllers\LicenseController@verifyLicense');
 
 
 
