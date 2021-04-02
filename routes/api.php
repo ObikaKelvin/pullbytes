0d3b6aacc8082fbf9b92810d1f86f21acff55a03
ObikaKelvin <obikakelvin.2255@gmail.com>
2021-04-02 19:31:30 +0100

    added verify license route

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/v1/login', 'App\Http\Controllers\AuthController@login')->middleware('api');
 Route::post('/v1/register', 'App\Http\Controllers\AuthController@register')->middleware('api');
+Route::post('/v1/verify_license', 'App\Http\Controllers\LicenseController@verifyLicense');
 
 
 
